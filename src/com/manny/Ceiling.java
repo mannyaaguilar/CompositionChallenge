f3a223a732f9a315850a333430641c4e2decc9d2
Manny Aguilar <mannyaaguilar@yahoo.com>
2019-01-05 22:04:43 -0700

    first commit

---
+++ src/com/manny/Ceiling.java
@@ -0,0 +1,19 @@
+package com.manny;
+
+public class Ceiling {
+    private int height;
+    private int paintColor;
+
+    public Ceiling(int height, int paintColor) {
+        this.height = height;
+        this.paintColor = paintColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintColor() {
+        return paintColor;
+    }
+}
---
+++ src/com/manny/Main.java
@@ -0,0 +1,31 @@
+package com.manny;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Challenge!
+        // Create a single room of a house using composition.
+        // Think about the things that should be included in the room.
+        // Maybe physical parts of the house, but furniture as well.
+        // Add at least one method to access an object via a getter and
+        // then add at least one method to hide the object e.g. not using a getter
+        // but to access the object used in composition within the main class.
+
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1  );
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedroom = new Bedroom("Mannys", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+        bedroom.makeBed();
+
+        bedroom.getLamp().turnOn();
+    }
+}
